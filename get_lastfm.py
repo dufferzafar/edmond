83913cc665df6d9cc001128da8a539c5886f103e
Shadab Zafar <dufferzafar0@gmail.com>
2015-08-27 23:15:32 +0530

    Last.fm: Fetch info (listeners, playcount) for each track

--- get_lastfm.py
+++ get_lastfm.py
@@ -3,18 +3,34 @@ import tabulate
 from api import lastfm
 
 # Our data store
-headers = ["Name", "MusicBrainz Identifier", "Duration"]
 data = []
+headers = [
+    "Name",
+    "MusicBrainz Identifier",
+    "Duration",
+    "Listeners",
+    "Playcount",
+]
 
-# A function that gets top tracks of a tag
+# Get top tracks of a tag
 tag_top = partial(lastfm.get, method='tag.getTopTracks')
+
+# Get trak info
+track_info = partial(lastfm.get, method='track.getInfo')
 
 # Let's get some data
 for page in range(1, 2):
-    r = tag_top(tag='electronic', page=page).json()
+    r = tag_top(tag='electronic', page=page, limit=25).json()
 
     for track in r['tracks']['track']:
         if track['mbid']:
-            data.append([track['name'], track['mbid'], track['duration']])
+            info = track_info(mbid=track['mbid']).json()
+            data.append([
+                track['name'],
+                track['mbid'],
+                info['track']['duration'],
+                info['track']['listeners'],
+                info['track']['playcount'],
+            ])
 
 print(tabulate.tabulate(data, headers=headers))
