5713e01d1d8010eab486b86154c3c023d3259ca7
Shadab Zafar <dufferzafar0@gmail.com>
2015-08-27 23:54:09 +0530

    Add some high level features to our last.fm tracks data

--- get_lastfm.py
+++ get_lastfm.py
@@ -1,6 +1,9 @@
 from functools import partial
+from simplejson.scanner import JSONDecodeError
+
 import tabulate
-from api import lastfm
+
+from api import lastfm, acousticbrainz
 
 # Our data store
 data = []
@@ -10,6 +13,10 @@ headers = [
     "Duration",
     "Listeners",
     "Playcount",
+    "AB - Female",
+    "AB - Bright",
+    "AB - Danceable",
+    "AB - Voice",
 ]
 
 # Get top tracks of a tag
@@ -18,6 +25,9 @@ tag_top = partial(lastfm.get, method='tag.getTopTracks')
 # Get trak info
 track_info = partial(lastfm.get, method='track.getInfo')
 
+# Get high level information from Acousticbrainz
+ab_high = acousticbrainz.get_high
+
 # Let's get some data
 for page in range(1, 2):
     r = tag_top(tag='electronic', page=page, limit=25).json()
@@ -25,12 +35,22 @@ for page in range(1, 2):
     for track in r['tracks']['track']:
         if track['mbid']:
             info = track_info(mbid=track['mbid']).json()
+
+            try:
+                ab = ab_high(mbid=track['mbid'])
+            except JSONDecodeError:
+                continue
+
             data.append([
                 track['name'],
                 track['mbid'],
                 info['track']['duration'],
                 info['track']['listeners'],
                 info['track']['playcount'],
+                ab['female'],
+                ab['bright'],
+                ab['danceable'],
+                ab['voice'],
             ])
 
 print(tabulate.tabulate(data, headers=headers))
