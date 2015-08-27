cef1e876be3ac9a417ad1c89deea7509b68b216b
Shadab Zafar <dufferzafar0@gmail.com>
2015-08-27 22:52:58 +0530

    Add interface to last.fm webservice

---
+++ api/lastfm.py
@@ -0,0 +1,14 @@
+import requests
+
+API_URL = 'http://ws.audioscrobbler.com/2.0/'
+API_KEY = '1e3f5c62ae6ea96b97fd4d4e00f4825a'
+
+
+def get(**kwargs):
+    """ Our interface to the last.fm webservice. """
+
+    # Set up parameters
+    params = dict(api_key=API_KEY, format='json', limit=100)
+    params.update(kwargs)
+
+    return requests.get(API_URL, params=params)
