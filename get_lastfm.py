26f4a8b60000be6883af8bcf3dd9aabea0462654
Shadab Zafar <dufferzafar0@gmail.com>
2015-08-30 17:51:08 +0530

    Move features dictionary part to a separate module

--- get_lastfm.py
+++ get_lastfm.py
@@ -7,72 +7,9 @@ from api import lastfm, acousticbrainz
 
 # Our data store
 data = []
-headers = [
-    "Name",
-    "MusicBrainz Identifier",
-    "Duration",
-    "Listeners",
-    "Playcount",
-    "AB - Acoustic",
-    "AB - Aggressive",
-    "AB - Danceable",
-    "AB - Dortmund",
-    "AB - Electronic",
-    "AB - Female",
-    "AB - Happy",
-    "AB - Party",
-    "AB - Relaxed",
-    "AB - Sad",
-    "AB - Timbre",
-    "AB - Tonal",
-    "AB - Voice",
-    "AB - Average Loudness",
-    "AB - Barkbands Crest",
-    "AB - Barkbands Kurtosis",
-    "AB - Barkbands Flatness dB",
-    "AB - Barkbands Skewness",
-    "AB - Barkbands Spread",
-    "AB - Dissonance",
-    "AB - Dynamic Complexity",
-    "AB - Erbbands Crest",
-    "AB - Erbbands Flatness dB",
-    "AB - Erbbands Kurtosis",
-    "AB - Erbbands Skewness",
-    "AB - Erbbands Spread",
-    "AB - HFC",
-    "AB - Melbands Crest",
-    "AB - Melbands Flatness dB",
-    "AB - Melbands Kurtosis",
-    "AB - Melbands Skewness",
-    "AB - Melbands Spread",
-    "AB - Pitch Salience",
-    "AB - Silence Rate 30dB",
-    "AB - Spectral Centroid",
-    "AB - Spectral Complexity",
-    "AB - Spectral Decrease",
-    "AB - Spectral Energy",
-    "AB - Spectral Entropy",
-    "AB - Spectral Flux",
-    "AB - Spectral RMS",
-    "AB - Spectral Kurtosis",
-    "AB - Spectral Skewness",
-    "AB - Spectral Spread",
-    "AB - Spectral Rolloff",
-    "AB - Spectral Strongpeak",
-    "AB - Zero Crossing Rate",
-]
-
 
 # Get top tracks of a tag
 tag_top = partial(lastfm.get, method='tag.getTopTracks')
-
-# Get trak info
-track_info = partial(lastfm.get, method='track.getInfo')
-
-# Get high level information from Acousticbrainz
-ab_high = acousticbrainz.get_high
-
-ab_low = acousticbrainz.get_low
 
 # Let's get some data
 for page in range(1, 2):
@@ -81,67 +18,5 @@ for page in range(1, 2):
     for track in r['tracks']['track']:
         if track['mbid']:
             info = track_info(mbid=track['mbid']).json()
-
-            try:
-                # 1 is high, 0 is low. Binary basics, people!
-                ab1 = ab_high(mbid=track['mbid'])
-                ab0 = ab_low(mbid=track['mbid'])
-            except JSONDecodeError:
-                continue
-
-            data.append([
-                track['name'],
-                track['mbid'],
-                info['track']['duration'],
-                info['track']['listeners'],
-                info['track']['playcount'],
-                ab1['acoustic'],
-                ab1['aggressive'],
-                ab1['danceable'],
-                ab1['dortmund'],
-                ab1['electronic'],
-                ab1['female'],
-                ab1['happy'],
-                ab1['party'],
-                ab1['relaxed'],
-                ab1['sad'],
-                ab1['timbre'],
-                ab1['tonal'],
-                ab1['voice'],
-                ab0['average_loudness'],
-                ab0['barkbands_crest'],
-                ab0['barkbands_kurtosis'],
-                ab0['barkbands_flatness_db'],
-                ab0['barkbands_skewness'],
-                ab0['barkbands_spread'],
-                ab0['dissonance'],
-                ab0['dynamic_complexity'],
-                ab0['erbbands_crest'],
-                ab0['erbbands_flatness_db'],
-                ab0['erbbands_kurtosis'],
-                ab0['erbbands_skewness'],
-                ab0['erbbands_spread'],
-                ab0['hfc'],
-                ab0['melbands_crest'],
-                ab0['melbands_flatness_db'],
-                ab0['melbands_kurtosis'],
-                ab0['melbands_skewness'],
-                ab0['melbands_spread'],
-                ab0['pitch_salience'],
-                ab0['silence_rate_30db'],
-                ab0['spectral_centroid'],
-                ab0['spectral_complexity'],
-                ab0['spectral_decrease'],
-                ab0['spectral_energy'],
-                ab0['spectral_entropy'],
-                ab0['spectral_flux'],
-                ab0['spectral_rms'],
-                ab0['spectral_kurtosis'],
-                ab0['spectral_skewness'],
-                ab0['spectral_spread'],
-                ab0['spectral_rolloff'],
-                ab0['spectral_strongpeak'],
-                ab0['zerocrossingrate'],
-            ])
 
 print(tabulate.tabulate(data, headers=headers))
---
+++ track.py
@@ -0,0 +1,64 @@
+def features(lfm, ab_high, ab_low):
+    """ Build features dictionary. """
+
+    rv = {
+        # Last.fm
+        "Name": lfm['track']['name'],
+        "MBID": lfm['track']['mbid'],
+        "Duration": lfm['track']['duration'],
+        "Listeners": lfm['track']['listeners'],
+        "Playcount": lfm['track']['playcount'],
+
+        # AcousticBrainz High Level Data
+        "ABH - Acoustic": ab_high['highlevel']['mood_acoustic']['all']['acoustic'],
+        "ABH - Aggressive": ab_high['highlevel']['mood_aggressive']['all']['aggressive'],
+        "ABH - Danceable": ab_high['highlevel']['danceability']['all']['danceable'],
+        "ABH - Dortmund": ab_high['highlevel']['genre_dortmund']['probability'],
+        "ABH - Electronic": ab_high['highlevel']['mood_electronic']['all']['electronic'],
+        "ABH - Female": ab_high['highlevel']['gender']['all']['female'],
+        "ABH - Happy": ab_high['highlevel']['mood_happy']['all']['happy'],
+        "ABH - Party": ab_high['highlevel']['mood_party']['all']['party'],
+        "ABH - Relaxed": ab_high['highlevel']['mood_relaxed']['all']['relaxed'],
+        "ABH - Sad": ab_high['highlevel']['mood_sad']['all']['sad'],
+        "ABH - Timbre": ab_high['highlevel']['timbre']['all']['bright'],
+        "ABH - Tonal": ab_high['highlevel']['tonal_atonal']['all']['tonal'],
+        "ABH - Voice": ab_high['highlevel']['voice_instrumental']['all']['voice'],
+
+        # AcousticBrainz Low Level Data
+        "ABL - Average Loudness": ab_low['lowlevel']['average_loudness'],
+        "ABL - Barkbands Crest": ab_low['lowlevel']['barkbands_crest']['mean'],
+        "ABL - Barkbands Kurtosis": ab_low['lowlevel']['barkbands_kurtosis']['mean'],
+        "ABL - Barkbands Flatness dB": ab_low['lowlevel']['barkbands_flatness_db']['mean'],
+        "ABL - Barkbands Skewness": ab_low['lowlevel']['barkbands_skewness']['mean'],
+        "ABL - Barkbands Spread": ab_low['lowlevel']['barkbands_spread']['mean'],
+        "ABL - Dissonance": ab_low['lowlevel']['dissonance']['mean'],
+        "ABL - Dynamic Complexity": ab_low['lowlevel']['dynamic_complexity'],
+        "ABL - Erbbands Crest": ab_low['lowlevel']['erbbands_crest']['mean'],
+        "ABL - Erbbands Flatness dB": ab_low['lowlevel']['erbbands_flatness_db']['mean'],
+        "ABL - Erbbands Kurtosis": ab_low['lowlevel']['erbbands_kurtosis']['mean'],
+        "ABL - Erbbands Skewness": ab_low['lowlevel']['erbbands_skewness']['mean'],
+        "ABL - Erbbands Spread": ab_low['lowlevel']['erbbands_spread']['mean'],
+        "ABL - HFC": ab_low['lowlevel']['hfc']['mean'],
+        "ABL - Melbands Crest": ab_low['lowlevel']['melbands_crest']['mean'],
+        "ABL - Melbands Flatness dB": ab_low['lowlevel']['melbands_flatness_db']['mean'],
+        "ABL - Melbands Kurtosis": ab_low['lowlevel']['melbands_kurtosis']['mean'],
+        "ABL - Melbands Skewness": ab_low['lowlevel']['melbands_skewness']['mean'],
+        "ABL - Melbands Spread": ab_low['lowlevel']['melbands_spread']['mean'],
+        "ABL - Pitch Salience": ab_low['lowlevel']['pitch_salience']['mean'],
+        "ABL - Silence Rate 30dB": ab_low['lowlevel']['silence_rate_30dB']['mean'],
+        "ABL - Spectral Centroid": ab_low['lowlevel']['spectral_centroid']['mean'],
+        "ABL - Spectral Complexity": ab_low['lowlevel']['spectral_complexity']['mean'],
+        "ABL - Spectral Decrease": ab_low['lowlevel']['spectral_decrease']['mean'],
+        "ABL - Spectral Energy": ab_low['lowlevel']['spectral_energy']['mean'],
+        "ABL - Spectral Entropy": ab_low['lowlevel']['spectral_entropy']['mean'],
+        "ABL - Spectral Flux": ab_low['lowlevel']['spectral_flux']['mean'],
+        "ABL - Spectral RMS": ab_low['lowlevel']['spectral_rms']['mean'],
+        "ABL - Spectral Kurtosis": ab_low['lowlevel']['spectral_kurtosis']['mean'],
+        "ABL - Spectral Skewness": ab_low['lowlevel']['spectral_skewness']['mean'],
+        "ABL - Spectral Spread": ab_low['lowlevel']['spectral_spread']['mean'],
+        "ABL - Spectral Rolloff": ab_low['lowlevel']['spectral_rolloff']['mean'],
+        "ABL - Spectral Strongpeak": ab_low['lowlevel']['spectral_strongpeak']['mean'],
+        "ABL - Zero Crossing Rate": ab_low['lowlevel']['zerocrossingrate']['mean'],
+    }
+
+    return rv
