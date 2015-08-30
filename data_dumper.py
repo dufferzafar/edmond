f02d2a6508e3de0c5e9bd013a4eb38f588df7562
Shadab Zafar <dufferzafar0@gmail.com>
2015-08-30 19:57:46 +0530

    Dumper: Send progress notifications to pushbullet

--- data_dumper.py
+++ data_dumper.py
@@ -1,23 +1,28 @@
 """ Fetch data from sources and dump to disk. """
 
 import os
-from functools import partial
-
 import json
 
-from api import lastfm
+from api import lastfm, pushbullet
 from track import get_info as track_info
 
+from config import (
+    PB_PARAMS,
+    PB_NOTIFY_WHEN,
+)
+
+# Folder to dump data to
 folder = 'dump'
 if not os.path.exists(folder):
     os.makedirs(folder)
 
-# Get top tracks of a tag
-tag_top = partial(lastfm.get, method='tag.getTopTracks')
+# Keep track of files dumped
+count_files = 0
 
 # Let's get some data
 for page in range(1, 2):
-    r = tag_top(tag='electronic', page=page, limit=10).json()
+    r = lastfm.get(method='tag.getTopTracks', tag='electronic',
+                   page=page, limit=5).json()
 
     for track in r['tracks']['track']:
 
@@ -26,5 +31,22 @@ for page in range(1, 2):
 
             # Dump data to disk!
             if info:
+                data = {
+                    'lfm': info[0],
+                    'ab_high': info[1],
+                    'ab_low': info[2],
+                }
+
                 with open('%s/%s' % (folder, track['mbid']), 'w') as output:
-                    json.dump({'lfm': info[0], 'ab_high': info[1], 'ab_low': info[2]}, output)
+                    json.dump(data, output)
+                    count_files += 1
+
+                    # Send progress to Pushbullet!
+                    if (count_files % PB_NOTIFY_WHEN) == 0:
+                        PB_PARAMS['body'] = PB_PARAMS['body'].format(count=count_files)
+                        pushbullet.send(**PB_PARAMS)
+
+# Game Over!
+PB_PARAMS['title'] = 'Finished: ' + PB_PARAMS['title']
+PB_PARAMS['body'] = PB_PARAMS['body'].format(count=count_files)
+pushbullet.send(**PB_PARAMS)
