a67f8210d8412ad2b07bbe89ea50d6df3319d723
Shadab Zafar <dufferzafar0@gmail.com>
2015-08-30 17:52:23 +0530

    Last.fm: Use the track module

--- get_lastfm.py
+++ get_lastfm.py
@@ -1,9 +1,7 @@
 from functools import partial
-from simplejson.scanner import JSONDecodeError
 
-import tabulate
-
-from api import lastfm, acousticbrainz
+from api import lastfm
+from track import get_info as track_info
 
 # Our data store
 data = []
@@ -13,10 +11,10 @@ tag_top = partial(lastfm.get, method='tag.getTopTracks')
 
 # Let's get some data
 for page in range(1, 2):
-    r = tag_top(tag='electronic', page=page, limit=25).json()
-
+    r = tag_top(tag='electronic', page=page, limit=5).json()
     for track in r['tracks']['track']:
         if track['mbid']:
-            info = track_info(mbid=track['mbid']).json()
+            info = track_info(mbid=track['mbid'])
+            if info:
+                data.append(info)
 
-print(tabulate.tabulate(data, headers=headers))
