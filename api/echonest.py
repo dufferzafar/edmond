f06176796a9a119c5f8f00b8ff00358cc2547920
kwikadi <kwikadi@gmail.com>
2015-10-25 17:46:15 +0530

    Vague/Lame changes

--- api/echonest.py
+++ api/echonest.py
@@ -1,7 +1,7 @@
 import pyen
 
-def get():
-	en = pyen.Pyen("HCMA5DHVIH502MXH1")
+def get(enest_key):
+	en = pyen.Pyen(enest_key)
 	id_response = en.get('song/search', title='my iron lung', artist='radiohead')
 
 	song_id = id_response['songs'][0]['id']
@@ -9,8 +9,3 @@ def get():
 	feature_response = en.get('track/profile', id=song_id, bucket='audio_summary')
 
 	return feature_response
-
-# if feature_response['track']['audio_summary']:
-# 	time_sign = feature_response['track']['audio_summary']['time_signature']
-# 	duration = feature_response['track']['audio_summary']['song_duration']
-# 	loudness = feature_response['track']['audio_summary']['loudness']
